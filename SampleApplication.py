2cecceb172a94aa5d1dbde04fbe99ad7db2511a1
Roberto Schiavone <hello@robertoschiavone.io>
2019-11-13 11:51:57 +0100

    The Tragedy of Darth Plagueis the Wise

--- SampleApplication.py
+++ SampleApplication.py
@@ -4,8 +4,8 @@ import AbstractApplication as Base
 class SampleApplication(Base.AbstractApplication):
     def main(self):
         self.setLanguage('en-US')
-        self.sayAnimated('Hello, world!')
-        sleep(2)
+        self.sayAnimated('Did you ever hear the tragedy of Darth Plagueis the Wise? I thought not. Its not a story the Jedi would tell you. Its a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life... He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful... the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic, he could save others from death, but not himself.')
+        # sleep(2)
 
     def onRobotEvent(self, event):
         print(event)
