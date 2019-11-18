f90daa894dd46a0bce7855de592d4c861f8f1fdc
MikeyQiu <scsse_qhs2015@126.com>
2019-11-19 00:37:05 +0100

    basic setting for google dialogflow

---
+++ dialogue sample.py
@@ -0,0 +1,60 @@
+import AbstractApplication as Base
+from threading import Semaphore
+
+
+class DialogFlowSampleApplication(Base.AbstractApplication):
+    def main(self):
+        # Set the correct language (and wait for it to be changed)
+        self.langLock = Semaphore(0)
+        self.setLanguage('en-US')
+        self.langLock.acquire()
+
+        # Pass the required Dialogflow parameters (add your Dialogflow parameters)
+        self.setDialogflowKey('nao-asipei-148b2e2fe841.json')
+        self.setDialogflowAgent('nao-asipei')
+
+        # Make the robot ask the question, and wait until it is done speaking
+        self.speechLock = Semaphore(0)
+        self.sayAnimated('Hello, what is your name?')
+        self.speechLock.acquire()
+
+        # Listen for an answer for at most 5 seconds
+        self.name = None
+        self.nameLock = Semaphore(0)
+        self.setAudioContext('answer_name')
+        self.startListening()
+        self.nameLock.acquire(timeout=5)
+        self.stopListening()
+        if not self.name:  # wait one more second after stopListening (if needed)
+            self.nameLock.acquire(timeout=1)
+
+        # Respond and wait for that to finish
+        if self.name:
+            self.sayAnimated('Nice to meet you ' + self.name + '!')
+        else:
+            self.sayAnimated('Sorry, I didn\'t catch your name.')
+        self.speechLock.acquire()
+
+        # Display a gesture (replace <gestureID> with your gestureID)
+        self.gestureLock = Semaphore(0)
+        self.doGesture('<gestureID>/behavior_1')
+        self.gestureLock.acquire()
+
+    def onRobotEvent(self, event):
+        if event == 'LanguageChanged':
+            self.langLock.release()
+        elif event == 'TextDone':
+            self.speechLock.release()
+        elif event == 'GestureDone':
+            self.gestureLock.release()
+
+    def onAudioIntent(self, *args, intentName):
+        if intentName == 'answer_name' and len(args) > 0:
+            self.name = args[0]
+            self.nameLock.release()
+
+
+# Run the application
+sample = DialogFlowSampleApplication()
+sample.main()
+sample.stop()
